e7623124a0e7ae33a35879d7f0cfbcb5c15c168f
Malte Linke (parzival-space) <me@parzival.space>
2025-04-16 19:11:44 +0200

    update FontAwesome icons in main.ts to include specific icons

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import './assets/main.css'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faGamepad, faHammer, faCodeBranch } from '@fortawesome/free-solid-svg-icons'
+import { faJava } from '@fortawesome/free-brands-svg-icons'
 
-library.add(fas, fab)
+library.add(faGamepad, faHammer, faCodeBranch, faJava)
 
 import { createApp } from 'vue'
 import App from './App.vue'
