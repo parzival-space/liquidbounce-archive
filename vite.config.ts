1370f30cb8d280122acfadb82ddec40f7f7d7842
Malte Linke (parzival-space) <me@parzival.space>
2025-04-16 19:14:42 +0200

    set base path in Vite config for production environment

--- vite.config.ts
+++ vite.config.ts
@@ -17,4 +17,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  base: process.env.NODE_ENV === 'production' ? '/liquidbounce-archive/' : '/',
 })
